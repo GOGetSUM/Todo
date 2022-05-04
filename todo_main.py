223822c8ffec2a2931cfd9947110b3b5fbc5def2
Nick <testcorona0429@gmail.com>
2022-05-04 14:22:55 -0700

    Initial Commitment. gitiignore

--- todo_main.py
+++ todo_main.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, redirect, url_for, flash, request
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, URL
@@ -12,7 +14,7 @@ from flask_bootstrap import Bootstrap
 #--------------SetUp App----------------------------------------------------------------
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('sk')
 Bootstrap(app)
 
 #-------------Connect DB-----------------------------------------------------------------
